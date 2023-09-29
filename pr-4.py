e94f43f721e7862aa8423185a08e59abfb6978ba
Диана Бачурина <145592752+yasher557@users.noreply.github.com>
2023-09-29 16:43:13 +0300

    Update pr-4.py

--- pr-4.py
+++ pr-4.py
@@ -1,7 +1,11 @@
+#  1
+
 a, b = int(input('введите число: ')), int(input('введите число: '))
 for i in range(a, b + 1):
     print(i)
 
+
+#  2
 
 a, b = int(input('введите число: ')), int(input('введите число: '))
 if a < b:
@@ -12,11 +16,15 @@ else:
         print(i)
 
 
+#  3
+
 a, b = int(input('введите число: ')), int(input('введите число: '))
 for i in range(a, b + 1, -1):
     if i % 2 != 0:
         print(i)
 
+
+#  4
 
 n = int(input('введите количество: '))
 summ = 0
@@ -25,6 +33,8 @@ for i in range(n + 1):
 print(summ)
 
 
+#  5
+
 n = int(input('введите число: '))
 summ = 0
 for i in range(n + 1):
@@ -32,12 +42,16 @@ for i in range(n + 1):
 print(summ)
 
 
+#  6
+
 n = int(input('введите число: '))
 fact = 1
 for i in range(1, n + 1):
     fact = fact * i
 print(fact)
 
+
+#  7
 
 n = int(input('введите число: '))
 summ = 0
@@ -47,6 +61,9 @@ for i in range(1, n + 1):
     summ += fact
 print(summ)
 
+
+#  8
+
 n = int(input('введите число: '))
 st = 0
 for i in range(1, n + 1):
@@ -55,6 +72,8 @@ for i in range(1, n + 1):
     print(st)
 
 
+#  9
+
 lst = [0, 1]
 n = int(input('введите число: '))
 for i in range(1, n + 1):
@@ -62,8 +81,10 @@ for i in range(1, n + 1):
 print(sum(lst))
 
 
+#  10
+
 lst = [0, 1]
-n, k = int(input('введите число: ')), int(input('введите число: '))
+n, k = int(input('введите число: ')), int(input('введите число: ')) - 1
 for i in range(1, n + 1):
     lst.append(lst[-2] + lst[-1])
 print(sum(lst[k: len(lst)]))
